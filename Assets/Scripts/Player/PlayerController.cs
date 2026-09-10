9579e8966608a419905205f19c7c79bcc7d13cf2
masterdavid250 <davidarielconcon@gmail.com>
2026-09-10 12:49:50 +0800

    Added Ui to Player Controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private UIController playerUI; 
     private PlayerInput playerInput;
     
     public InputAction MoveAction { get; private set; }
