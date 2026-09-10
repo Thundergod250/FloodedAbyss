bd6fc5003f0969446caf6d064e4262e998b5ce5a
masterdavid250 <davidarielconcon@gmail.com>
2026-09-10 13:07:09 +0800

    Added Global Game Manager Values

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // Singleton instance
+    public static GameManager Instance { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Global References")]
+    public UIController uiController;
+    public PlayerController playerController;
+
+    private void Awake()
     {
-        
+        // Ensure only one GameManager exists
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // Persist across scenes
     }
 }
