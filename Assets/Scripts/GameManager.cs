dd2c44f1c967acceef709814b4d06d67a3af46bb
masterdavid250 <davidarielconcon@gmail.com>
2026-09-10 16:48:32 +0800

    Added Resource Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    // Singleton instance
     public static GameManager Instance { get; private set; }
 
     [Header("Global References")]
     public UIController uiController;
     public PlayerController playerController;
+    public ResourceManager resourceManager; 
 
     private void Awake()
     {
-        // Ensure only one GameManager exists
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -19,6 +19,6 @@ public class GameManager : MonoBehaviour
         }
 
         Instance = this;
-        DontDestroyOnLoad(gameObject); // Persist across scenes
+        DontDestroyOnLoad(gameObject); 
     }
 }
