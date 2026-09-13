791fa5253821ae2841915896819223d321dee8af
Jeremy Kurt Tan <jeremykurttan@yahoo.com>
2026-09-13 17:40:45 +0800

    Added Currency Increase when on Rock Destruction
    placed yellow enemy in main scene, as well as connected some variables for the text update

--- Assets/Scripts/Objects/TemporaryBullet.cs
+++ Assets/Scripts/Objects/TemporaryBullet.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 public class TemporaryBullet : MonoBehaviour
 {
     [SerializeField] private float lifetime = 3f;
-    [SerializeField] private int damage ;
+    [SerializeField] private int damage;
+
+    private PlayerResources playerResources;
+
+    public void SetPlayerResources(PlayerResources resourceScript)
+    {
+        playerResources = resourceScript;
+    }
 
     private void Start()
     {
@@ -15,6 +22,11 @@ public class TemporaryBullet : MonoBehaviour
         if (collision.gameObject.TryGetComponent<Health>(out Health goHealth))
         {
             goHealth.TakeDamage(damage);
+        }
+
+        if (collision.gameObject.TryGetComponent<EnemyDrops>(out EnemyDrops drops))
+        {
+            drops.GainPlayerReference(playerResources);
         }
 
         Debug.Log("Hit: " + collision.gameObject.name);
