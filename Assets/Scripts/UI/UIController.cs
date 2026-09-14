98eec030095dfca971e6bc253e93cb38487dfc8f
JCAtienza <johnchristian.atienza@benilde.edu.ph>
2026-09-14 13:19:45 +0800

    Revert "NPC Interaction"
    This reverts commit 2bc5b631e51eded0b64d945f52bb87ff2286cabc.

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
-using TMPro;
 
 public class UIController : MonoBehaviour
 {
     public static UIController Instance { get; private set; }
 
-    [Header("Interaction Prompt")]
     [SerializeField] private UIInteraction uiInteraction;
-
-    [Header("Dialogue System")]
-    [SerializeField] private GameObject dialoguePanel;
-    [SerializeField] private TextMeshProUGUI dialogueTextComponent;
 
     private void Awake()
     {
@@ -28,27 +22,6 @@ public class UIController : MonoBehaviour
         if (uiInteraction != null)
         {
             uiInteraction.SetUIActive(isVisible);
-        }
-    }
-
-    public void ShowDialogue(string text)
-    {
-        if (dialoguePanel != null)
-        {
-            dialoguePanel.SetActive(true);
-        }
-
-        if (dialogueTextComponent != null)
-        {
-            dialogueTextComponent.text = text;
-        }
-    }
-
-    public void HideDialogue()
-    {
-        if (dialoguePanel != null)
-        {
-            dialoguePanel.SetActive(false);
         }
     }
 }
