e10613163e3ec91fdb27ed06661be26ce19ff152
masterdavid250 <davidarielconcon@gmail.com>
2026-09-19 17:52:47 +0800

    Fixed Ui Interaction Script to allow Dynamic Changes

--- Assets/Scripts/UI/UIInteraction.cs
+++ Assets/Scripts/UI/UIInteraction.cs
@@ -1,14 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class UIInteraction : MonoBehaviour
 {
     [SerializeField] private GameObject interactionPanel;
+    [SerializeField] private TextMeshPro interactionText;
 
     public void SetUIActive(bool isActive)
     {
-        if (interactionPanel != null)
-        {
+        if (interactionPanel != null) 
             interactionPanel.SetActive(isActive);
-        }
+    }
+
+    public void SetText(string text)
+    {
+        if (interactionText != null) 
+            interactionText.text = text;
     }
 }
