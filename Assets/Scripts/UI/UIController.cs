84ab13d3fdaa8014726251f444213d03fd1ceab0
JCAtienza <johnchristian.atienza@benilde.edu.ph>
2026-09-19 22:50:05 +0800

    Removed function in UIController

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -97,18 +97,6 @@ public class UIController : MonoBehaviour
         Cursor.lockState = showCursor ? CursorLockMode.None : CursorLockMode.Locked;
     }
 
-    public T GetModal<T>() where T : UiModals
-    {
-        foreach (var modal in modalDictionary.Values)
-        {
-            if (modal is T typedModal)
-            {
-                return typedModal;
-            }
-        }
-        return null;
-    }
-
     public void CloseAllModals()
     {
         OpenModal(UIState.HuD);
