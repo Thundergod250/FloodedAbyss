701c80704cf00dad1cacc770148e5b688c895cb2
masterdavid250 <davidarielconcon@gmail.com>
2026-09-24 13:46:35 +0800

    Revert "Split UI Panels into their own prefab"
    This reverts commit e9cf4f64f971768e919da8c3d3962d85f9baedd1.

--- Assets/Scripts/Objects/Structures/ItemStructures.cs
+++ Assets/Scripts/Objects/Structures/ItemStructures.cs
@@ -1,42 +1,16 @@
 using UnityEngine;
 
-public class ItemStructure : Item
+public class ItemStructures : MonoBehaviour
 {
-    [Header("Structure Settings")]
-    [SerializeField] private StructureDataSO structureData;
-
-    protected PlayerResources playerResources;
-
-    protected virtual void Start()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        if (GameManager.Instance != null && GameManager.Instance.playerController != null)
-        {
-            playerResources = GameManager.Instance.playerController.PlayerResources;
-        }
+        
     }
 
-    /// <summary>
-    /// Triggered when the player interacts with this placed structure.
-    /// Opens the Structure UI Modal and passes necessary structure references/data.
-    /// </summary>
-    public override void Activate()
+    // Update is called once per frame
+    void Update()
     {
-        // 1. Change UI State to Structure
-        GameManager.Instance.uiController.OpenModal(UIController.UIState.Structure);
-
-        // 2. Retrieve the UIStructure modal script instance
-        UIStructures structureModal = GameManager.Instance.uiController.GetModal<UIStructures>(UIController.UIState.Structure);
-
-        if (structureModal != null)
-        {
-            // 3. Populate or bind structure data to the UI
-            //structureModal.SetupStructure(this, structureData);
-        }
-        else
-        {
-            Debug.LogError($"[ItemStructure] UIStructure modal instance not found on UIState.Structure!");
-        }
+        
     }
-
-    public StructureDataSO StructureData => structureData;
 }
